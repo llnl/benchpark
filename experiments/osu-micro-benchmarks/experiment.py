4601fe83d423fc741c0ae45192e2155b7f152a0f
august-knox <knox10@llnl.gov>
2024-11-13 14:48:27 -0800

    updating experiment class

--- experiments/osu-micro-benchmarks/experiment.py
+++ experiments/osu-micro-benchmarks/experiment.py
@@ -1,17 +1,9 @@
 from benchpark.directives import variant
 from benchpark.error import BenchparkError
 from benchpark.experiment import Experiment
-from benchpark.scaling import StrongScaling
-import re
 
 
 class OsuMicroBenchmarks(Experiment):
-
-    variant(
-        "version",
-        default="develop",
-        description="app version",
-    )
 
     variant(
         "workload",
@@ -92,8 +84,7 @@ class OsuMicroBenchmarks(Experiment):
     )
 
     def compute_applications_section(self):
-	scaling_modes = {
-            "strong": self.spec.satisfies("strong=oui"),
+        scaling_modes = {
             "single_node": self.spec.satisfies("single_node=oui"),
         }
 
@@ -104,70 +95,17 @@ class OsuMicroBenchmarks(Experiment):
             )
 
         variables = {}
-	num_nodes = {"n_nodes", 1}
-        variables["scaling_nodes"] = "2"
-        variables["n_nodes"] = "{scaling_nodes}"
-        variables["n_ranks_per_node"] = "36"
+        num_nodes = {"n_nodes": 2}
 
-        pattern = r"workload=(?P<workload>[^ ]+)\s*"
-        workload = re.search(pattern, str(self.spec.variants))
-        workload_string = workload.group(1)
-
-	if self.spec.satisfies("single_node=oui"):
+        if self.spec.satisfies("single_node=oui"):
             for pk, pv in num_nodes.items():
                 self.add_experiment_variable(pk, pv, True)
 
-	elif self.spec.satisfies("strong=oui"):
-            scaled_variables = self.generate_strong_scaling_params(
-                {tuple(num_nodes.keys()): list(num_nodes.values())},
-                int(self.spec.variants["scaling-factor"][0]),
-                int(self.spec.variants["scaling-iterations"][0]),
-            )
-            for pk, pv in scaled_variables.items():
-                self.add_experiment_variable(pk, pv, True)
-       # return {
-        #    "osu-micro-benchmarks": {
-         #       # TODO replace with a hash once we have one?
-          #      "workloads": {
-           #         workload_string: {
-            #            "experiments": {
-             #               "scaling_{n_nodes}nodes_medium": {
-	#		        "variants": {
-         #                           "package_manager": "spack",
-          #                      },
-           #                     "variables": variables,
-            #                }
-             #           }
-              #      }
-               # }
-           # }
-        }
-
     def compute_spack_section(self):
-	app_version = self.spec.variants["version"][0]
-        # get system config options
-        # TODO: Get compiler/mpi/package handles directly from system.py
         system_specs = {}
         system_specs["compiler"] = "default-compiler"
         system_specs["mpi"] = "default-mpi"
-        # set package spack specs
-        # empty package_specs value implies external package
         self.add_spack_spec(system_specs["mpi"])
         self.add_spack_spec(
-            self.name, [f"osu-micro-benchmarks@{app_version}", system_specs["compiler"]]
+            self.name, [f"osu-micro-benchmarks", system_specs["compiler"]]
         )
-        # TODO: express that we need certain variables from system
-        # Does not need to happen before merge, separate task
-        #osu_microbenchmarks_spack_spec = "osu-micro-benchmarks"
-       # packages = ["default-mpi", self.spec.name]
-
-#        return {
- #           "packages": {
-  #              "osu-micro-benchmarks": {
-   #                 "pkg_spec": osu_microbenchmarks_spack_spec,
-    #                "compiler": "default-compiler",
-     #           }
-      #      },
-       #     "environments": {"osu-micro-benchmarks": {"packages": packages}},
-            
-       # }
