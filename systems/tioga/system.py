59bca77d291ae446735d4e9333eeb7967160ded5
pearce8 <pearce8@llnl.gov>
2024-11-14 12:01:53 -0600

    lint

--- systems/tioga/system.py
+++ systems/tioga/system.py
@@ -33,7 +33,7 @@ class Tioga(System):
 
     def initialize(self):
         super().initialize()
-        if (self.spec.variants["scheduler"][0]):
+        if self.spec.variants["scheduler"][0]:
             self.scheduler = "flux"
         self.sys_cores_per_node = "64"
         self.sys_gpus_per_node = "4"
