13f2605f541c2e3f8955942dbba73d7969898b6a
Peter Josef Scheibel <scheibel1@llnl.gov>
2024-12-02 16:40:13 -0800

    pull in static 7a/6a configs

--- systems/aws-pcluster/system.py
+++ systems/aws-pcluster/system.py
@@ -17,21 +17,63 @@ id_to_resources = {
         "sys_cores_per_node": 4,
         "sys_mem_per_node": 8,
     },
+    "hpc7a.48xlarge": {
+        "sys_cores_per_node": 96,
+        "sys_mem_per_node": 768,
+    },
+    "hpc6a.48xlarge": {
+        "sys_cores_per_node": 96,
+        "sys_mem_per_node": 384,
+    },
 }
 
 
 class Aws(System):
     variant(
         "instance_type",
-        values=("c6g.xlarge", "c4.xlarge"),
+        values=("c6g.xlarge", "c4.xlarge", "hpc7a.48xlarge", "hpc6a.48xlarge"),
         default="c4.xlarge",
         description="AWS instance type",
     )
 
     def initialize(self):
         super().initialize()
-        self.scheduler = "mpi"
+        self.scheduler = "slurm"
         # TODO: for some reason I have to index to get value, even if multi=False
         attrs = id_to_resources.get(self.spec.variants["instance_type"][0])
         for k, v in attrs.items():
             setattr(self, k, v)
+
+    def external_pkg_configs(self):
+        externals = LlnlElcapitan.resource_location / "externals"
+
+        selections = [
+            externals / "base" / "00-packages.yaml",
+        ]
+
+        return selections
+
+    def compiler_configs(self):
+        compilers = LlnlElcapitan.resource_location / "compilers"
+
+        selections = [
+            compilers / "gcc" / "00-gcc-7-compilers.yaml",
+        ]
+
+        return selections
+
+    def sw_description(self):
+        return """\
+software:
+  packages:
+    default-compiler:
+      pkg_spec: gcc@7.3.1
+    default-mpi:
+      pkg_spec: openmpi@4.1.5%gcc@7.3.1
+    compiler-gcc:
+      pkg_spec: gcc@7.3.1
+    lapack:
+      pkg_spec: lapack@3.4.2
+    mpi-gcc:
+      pkg_spec: openmpi@4.1.5%gcc@7.3.1
+"""
