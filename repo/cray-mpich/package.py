1783331174aafe91609a57488ce4bfd74c3a4c73
Riyaz Haque <haque1@llnl.gov>
2025-01-15 10:50:04 -0700

    venado system specs

--- repo/cray-mpich/package.py
+++ repo/cray-mpich/package.py
@@ -17,7 +17,7 @@ class CrayMpich(BuiltinCM):
 
         if self.spec.satisfies("+gtl"):
             gtl_lib_prefix = self.spec.extra_attributes["gtl_lib_path"]
-            libraries = ["libmpi_gtl_hsa"]
+            libraries = ["libmpi_gtl_hsa", "libmpi_gtl_cuda"]
             libs += find_libraries(libraries, root=gtl_lib_prefix, recursive=True)
 
         return libs
@@ -29,8 +29,6 @@ class CrayMpich(BuiltinCM):
         if self.spec.satisfies("+gtl"):
             env.set("MPICH_GPU_SUPPORT_ENABLED", "1")
             env.prepend_path("LD_LIBRARY_PATH", self.spec.extra_attributes["gtl_lib_path"])
-            env.set("GTL_HSA_VSMSG_CUTOFF_SIZE", str(self.spec.extra_attributes["gtl_cutoff_size"]))
-            env.set("FI_CXI_ATS", str(self.spec.extra_attributes["fi_cxi_ats"]))
         else:
             env.set("MPICH_GPU_SUPPORT_ENABLED", "0")
             gtl_path = self.spec.extra_attributes.get("gtl_lib_path", "")
