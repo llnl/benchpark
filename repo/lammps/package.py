e75666fbaa3dae9831391b1b3f08a31070b5f369
Riyaz Haque <haque1@llnl.gov>
2025-01-17 19:13:17 -0700

    lammps cuda

--- repo/lammps/package.py
+++ repo/lammps/package.py
@@ -13,8 +13,10 @@ class Lammps(BuiltinLammps):
   depends_on("kokkos+rocm", when="+rocm")
   depends_on("kokkos+cuda", when="+cuda")
 
-  def setup_run_environment(self, env):
+  conflicts("+rocm", when="+cuda")
+  conflicts("+cuda", when="+rocm")
 
+  def setup_run_environment(self, env):
     super(BuiltinLammps, self).setup_run_environment(env)
 
     if self.compiler.extra_rpaths:
@@ -22,9 +24,19 @@ class Lammps(BuiltinLammps):
         env.prepend_path("LD_LIBRARY_PATH", rpath)
 
   def setup_build_environment(self, env):
-    super().setup_build_environment(env)
+    super(BuiltinLammps, self).setup_build_environment(env)
 
-    spec = self.spec
-    if "+mpi" in spec:
-      if spec["mpi"].extra_attributes and "ldflags" in spec["mpi"].extra_attributes:
-        env.append_flags("LDFLAGS", spec["mpi"].extra_attributes["ldflags"])
+    if "+cuda" in self.spec:
+      env.set("NVCC_APPEND_FLAGS", "-allow-unsupported-compiler")
+
+  def cmake_args(self):
+    args = super(BuiltinLammps, self).cmake_args()
+    args.append(f"-DMPI_CXX_LINK_FLAGS='{self.spec['mpi'].libs.ld_flags}'")
+
+    cflags = self.spec.compiler_flags['cflags']
+    args.append(f"-DCMAKE_C_FLAGS={' '.join(cflags)}")
+
+    cxxflags = self.spec.compiler_flags['cxxflags']
+    args.append(f"-DCMAKE_CXX_FLAGS={' '.join(cxxflags)}")
+
+    return args
