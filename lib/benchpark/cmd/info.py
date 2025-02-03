1737c371f2bc8c814afa9d45daa5947931c0f7b4
Michael Richard Mckinsey <mckinsey@ruby966.llnl.gov>
2025-02-03 15:12:46 -0800

    Refactor info_system for clarity

--- lib/benchpark/cmd/info.py
+++ lib/benchpark/cmd/info.py
@@ -9,56 +9,56 @@ def gen_header(name):
     print("=" * 20 + f"\n=== {name}\n" + "=" * 20)
 
 
-def info_system_system_site(system_spec):
-    gen_header("System Site")
-    print(system_spec.system_class.system_site)
-
-
-def info_system_maintainer(system_spec):
-    gen_header("Maintainer")
-    print(system_spec.system_class.maintainer)
-
-
-def info_system_variants(system_spec):
-    gen_header("Variants")
-    # Dict with one value
-    all_variants = list(system_spec.system_class.variants.values())[0]
-    for var in all_variants.values():
-        for attr in var.__dict__.keys():
-            print(attr + ":", var.__dict__[attr])
-        print()
-        # print(var.name+":", var.values)
-
-
-def info_system_hardware(system_spec):
-    gen_header("Hardware")
-    for cluster, resource_dict in system_spec.system_class.id_to_resources.items():
-        print("For cluster", cluster + ":")
-        for resource_key, resource_value in resource_dict.items():
-            if isinstance(resource_value, str) and ".yaml" in resource_value:
-                print("\t" + resource_key + ":")
-                data = yaml.safe_load(open(resource_value, "r"))
-                print(textwrap.indent(yaml.dump(data), "\t"))
-            else:
-                print("\t" + resource_key + ":", resource_value)
-
-
 def info_system(args):
+    def _info_system_system_site(system_spec):
+        gen_header("System Site")
+        print(system_spec.system_class.system_site)
+
+    def _info_system_maintainer(system_spec):
+        gen_header("Maintainer")
+        print(system_spec.system_class.maintainer)
+
+    def _info_system_variants(system_spec):
+        gen_header("Variants")
+        all_variants = list(system_spec.system_class.variants.values())[0]
+        for var in all_variants.values():
+            for attr, value in var.__dict__.items():
+                print(f"{attr}: {value}")
+            print()
+
+    def _info_system_hardware(system_spec):
+        gen_header("Hardware")
+        for cluster, resource_dict in system_spec.system_class.id_to_resources.items():
+            print(f"For cluster {cluster}:")
+            for resource_key, resource_value in resource_dict.items():
+                if isinstance(resource_value, str) and ".yaml" in resource_value:
+                    print(f"\t{resource_key}:")
+                    with open(resource_value, "r") as f:
+                        data = yaml.safe_load(f)
+                        print(textwrap.indent(yaml.dump(data), "\t"))
+                else:
+                    print(f"\t{resource_key}: {resource_value}")
+
     system_spec = benchpark.spec.SystemSpec(" ".join(args.spec))
 
-    if args.system_site:
-        info_system_system_site(system_spec)
-    if args.maintainer:
-        info_system_maintainer(system_spec)
-    if args.variants:
-        info_system_variants(system_spec)
-    if args.hardware:
-        info_system_hardware(system_spec)
-    if not args.system_site and not args.variants and not args.hardware:
-        info_system_system_site(system_spec)
-        info_system_maintainer(system_spec)
-        info_system_variants(system_spec)
-        info_system_hardware(system_spec)
+    # Map argument flags to functions
+    actions = {
+        "system_site": _info_system_system_site,
+        "maintainer": _info_system_maintainer,
+        "variants": _info_system_variants,
+        "hardware": _info_system_hardware,
+    }
+
+    # Call functions for enabled options, or all if no flag is set
+    any_flag_set = False
+    for flag, action in actions.items():
+        if getattr(args, flag, False):
+            action(system_spec)
+            any_flag_set = True
+
+    if not any_flag_set:
+        for action in actions.values():
+            action(system_spec)
 
 
 def setup_parser(root_parser):
