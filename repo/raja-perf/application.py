ab1535ea88fc950082a9e4e79e785a7cff6fb908
Michael Richard Mckinsey <mckinsey@lassen708.coral.llnl.gov>
2025-02-10 20:07:49 -0800

    Remove extra whitespace

--- repo/raja-perf/application.py
+++ repo/raja-perf/application.py
@@ -31,4 +31,3 @@ class RajaPerf(ExecutableApplication):
     figure_of_merit('All tests pass', log_file='{experiment_run_dir}/{experiment_name}.out', fom_regex=r'(?P<tpass>DONE)!!!...', group_name='tpass', units='')
 
     success_criteria('pass', mode='string', match=r'DONE!!!....', file='{experiment_run_dir}/{experiment_name}.out')
-   
