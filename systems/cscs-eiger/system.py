2d1f6a1de12e95bc5e2bbc531ff980e9b4558c99
august-knox <knox10@llnl.gov>
2025-02-11 16:46:18 -0800

    lint

--- systems/cscs-eiger/system.py
+++ systems/cscs-eiger/system.py
@@ -63,7 +63,7 @@ class CscsEiger(System):
         will fail if these variables are not defined though, so for now
         they are still generated (but with more-generic values).
         """
-        return f"""\
+        return """\
 software:
   packages:
     default-compiler:
