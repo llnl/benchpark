d68e75fb91eb1c697fbd41c2f297be4e5711e912
pearce8 <pearce8@llnl.gov>
2025-02-18 14:34:04 -0800

    Update experiment.py

--- experiments/qws/experiment.py
+++ experiments/qws/experiment.py
@@ -22,6 +22,7 @@ class Qws(Experiment, OpenMPExperiment):
         description="app version",
     )
 
+    maintainers("jdomke", "SBA0486")
     url = "https://www.riken.jp/en/research/labs/r-ccs/field_theor/index.html"
 
     def compute_applications_section(self):
