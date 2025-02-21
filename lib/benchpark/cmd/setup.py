8514b0a4fdb7514ac23bde6441823b5d41743714
Mckinsey <mckinsey1@thursday.the-lab.llnl.gov>
2025-02-21 15:48:52 -0800

    Remove legacy modifier config

--- lib/benchpark/cmd/setup.py
+++ lib/benchpark/cmd/setup.py
@@ -110,8 +110,8 @@ def command(args):
 
     print(f"Setting up configs for Ramble workspace {ramble_configs_dir}")
 
-    legacy_modifier_config_dir = (
-        source_dir / "legacy" / "modifiers" / modifier / "configs"
+    modifier_config_dir = (
+        source_dir / str(experiment_id)
     )
     ramble_configs_dir.mkdir(parents=True)
     ramble_logs_dir.mkdir(parents=True)
@@ -128,7 +128,7 @@ def command(args):
 
     symlink_tree(configs_src_dir, ramble_configs_dir, include_fn)
     symlink_tree(experiment_src_dir, ramble_configs_dir, include_fn)
-    symlink_tree(legacy_modifier_config_dir, ramble_configs_dir, include_fn)
+    symlink_tree(modifier_config_dir, ramble_configs_dir, include_fn)
     symlink_tree(
         source_dir / "legacy" / "systems" / "common",
         ramble_spack_experiment_configs_dir,
@@ -163,7 +163,6 @@ def command(args):
         ramble(f"repo add --scope=site {source_dir}/repo")
         ramble('config --scope=site add "config:disable_progress_bar:true"')
         ramble(f"repo add -t modifiers --scope=site {source_dir}/modifiers")
-        ramble(f"repo add -t modifiers --scope=site {source_dir}/legacy/modifiers")
         ramble("config --scope=site add \"config:spack:global:args:'-d'\"")
 
     if not initializer_script.exists():
