28ae37ed90fbbed1131701bfc28ade8c43dc88c9
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-03-06 15:53:22 -0800

    Add basic tests that will fail if commands do not run

---
+++ lib/benchpark/test/commands.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import subprocess
+
+def test_list():
+    subprocess.run(["benchpark", "list"], check=True)
+
+def test_tags():
+    subprocess.run([
+        "benchpark",
+        "tags",
+        "-a",
+        "ad"
+    ], check=True)
