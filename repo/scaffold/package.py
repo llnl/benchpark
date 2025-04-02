7259757462c2003431b1711ffe6238f3559250fb
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-04-01 17:07:51 -0700

    Using pip instead of spack

--- repo/scaffold/package.py
+++ repo/scaffold/package.py
@@ -1,27 +1,29 @@
-# Copyright 2023 Lawrence Livermore National Security, LLC and other
-# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: Apache-2.0
+# # Copyright 2023 Lawrence Livermore National Security, LLC and other
+# # Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+# #
+# # SPDX-License-Identifier: Apache-2.0
 
-from spack import *
+# from glob import glob
+
+# from spack import *
 
 
-class Scaffold(ROCmPackage, PythonExtension):
+# class Scaffold(Package, ROCmPackage, PythonExtension):
 
-    git = "https://github.com/LBANN/ScaFFold"
+#     #git = "https://github.com/LBANN/ScaFFold"
 
-    version("develop", branch="main")
+#     version("develop", branch="main")
 
-    # depends_on("rocm@6.2.1")
-    # depends_on("rocmcc@6.2.1-cce-18.0.1a-magic")
-    # depends_on("python@3.9.12")
+#     # depends_on("rocm@6.2.1")
+#     # depends_on("rocmcc@6.2.1-cce-18.0.1a-magic")
+#     # # depends_on("python@3.9.12")
 
-    @run_after("install")
-    def install_python(self):
-        # do LAMMPS Python package installation using pip
-        # if self.spec.satisfies("@20230328: +python"):
-        # with working_dir("python"):
-        #     os.environ["LAMMPS_VERSION_FILE"] = join_path(
-        #         self.stage.source_path, "src", "version.h"
-        #     )
-        pip(*PythonPipBuilder.std_args(self), f"--prefix={self.prefix}", ".")
+#     def install(self):
+#         # do LAMMPS Python package installation using pip
+#         # if self.spec.satisfies("@20230328: +python"):
+#         # with working_dir("python"):
+#         #     os.environ["LAMMPS_VERSION_FILE"] = join_path(
+#         #         self.stage.source_path, "src", "version.h"
+#         #     )
+#         print("########TEST")
+#         pip(*PythonPipBuilder.std_args(self), f"--prefix={self.prefix} --extra-index-url=https://download.pytorch.org/whl/rocm6.2", ".")
