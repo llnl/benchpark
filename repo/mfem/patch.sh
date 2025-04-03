923ddd8ddf74b3cdfea98d589426e9b050c86b46
Riyaz Haque <5333387+rfhaque@users.noreply.github.com>
2025-04-03 11:19:48 -0700

    Delete repo/mfem/patch.sh

--- repo/mfem/patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for url in $(curl -s https://github.com/spack/spack/tree/develop/var/spack/repos/builtin/packages/mfem | grep -o 'href="[^"]*\.patch"' | cut -d'"' -f2); do
-    raw_url="https://raw.githubusercontent.com${url/tree/develop/raw/develop}"
-    curl -O "$raw_url"
-done
