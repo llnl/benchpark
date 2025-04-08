978409128161f6d29bfcbc8d3cc63a585d186d6d
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-04-08 15:58:28 -0700

    Add more parameters

--- experiments/mpi-pingpong/experiment.py
+++ experiments/mpi-pingpong/experiment.py
@@ -21,8 +21,8 @@ class MpiPingpong(Experiment, Caliper):
 
         expr_vars = {
             "n_ranks": 2,
-            "iterations": 10,
-            "msg_size": 10
+            "iterations": [10,100,1000,10000,100000],
+            "msg_size": [16,256,4096,65536,1048576]
             # TODO: other expr vars?
         }
 
