3280fc7031319d2337b579ff1a5641094c877e52
Stephanie Christine Lam <lam44@dane1.llnl.gov>
2025-04-24 10:28:37 -0700

    ran with more ranks

--- experiments/mpi-pingpong/experiment.py
+++ experiments/mpi-pingpong/experiment.py
@@ -6,9 +6,10 @@
 from benchpark.directives import variant, maintainers
 from benchpark.experiment import Experiment
 from benchpark.caliper import Caliper
+from benchpark.affinity import Affinity
 
 
-class MpiPingpong(Experiment, Caliper):
+class MpiPingpong(Experiment, Caliper, Affinity):
 
     variant(
         "workload",
@@ -20,10 +21,13 @@ class MpiPingpong(Experiment, Caliper):
     def compute_applications_section(self):
 
         expr_vars = {
-            "n_ranks": 2,
-            "iterations": [10]*5+[10000]*5+[100000]*5,
-            "msg_size": [16,256,4096,65536,1048576]*3
+            "n_ranks": [112]*5, #[113]*5,
+            #"n_nodes" : 2,
+            #"iterations": [10]*5+[10000]*5+[100000]*5,
+            #"msg_size": [16,256,4096,65536,1048576]*3
             # TODO: other expr vars?
+            "iterations" : [1000000]*5, #[1000000]*5,
+            "msg_size" : [16000, 32000, 64000, 128000, 256000]*1      #[16,256,4096,65536,1048576]*1
         }
 
         for pk, pv in expr_vars.items():
