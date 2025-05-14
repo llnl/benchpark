8e67fa8501203fcd214b83b0a8b30badb5c9022f
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-05-14 14:14:44 -0700

    lammps strong scaling and variables, but no caliper to test

--- experiments/lammps/experiment.py
+++ experiments/lammps/experiment.py
@@ -5,6 +5,7 @@
 
 from benchpark.directives import variant, maintainers
 from benchpark.experiment import Experiment
+from benchpark.scaling import StrongScaling
 from benchpark.openmp import OpenMPExperiment
 from benchpark.cuda import CudaExperiment
 from benchpark.rocm import ROCmExperiment
@@ -12,6 +13,7 @@ from benchpark.rocm import ROCmExperiment
 
 class Lammps(
     Experiment,
+    StrongScaling,
     OpenMPExperiment,
     CudaExperiment,
     ROCmExperiment,
@@ -67,21 +69,27 @@ class Lammps(
         input_sizes = " ".join(f"-v {k} {{{k}}}" for k in problem_sizes.keys())
 
         if self.spec.satisfies("+openmp"):
-            self.add_experiment_variable("n_nodes", 1, True)
-            self.add_experiment_variable("n_ranks_per_node", 36, True)
+            n_resources = {"n_nodes": 1, "n_ranks_per_node": 36}
             self.add_experiment_variable("n_threads_per_proc", 1, True)
         elif self.spec.satisfies("+rocm"):
-            self.add_experiment_variable("n_nodes", 8, True)
-            self.add_experiment_variable("n_ranks_per_node", 8, True)
-            self.add_experiment_variable("n_gpus", 64, True)
+            n_resources = {"n_nodes": 8, "n_ranks_per_node": 8, "n_gpus": 64}
         elif self.spec.satisfies("+cuda"):
-            self.add_experiment_variable("n_nodes", 4, True)
-            self.add_experiment_variable("n_ranks_per_node", 4, True)
-            self.add_experiment_variable("n_gpus", 16, True)
+            n_resources = {"n_nodes": 4, "n_ranks_per_node": 4, "n_gpus": 16}
         else:
-            self.add_experiment_variable("n_nodes", 1, True)
-            self.add_experiment_variable("n_ranks_per_node", 36, True)
+            n_resources = {"n_nodes": 1, "n_ranks_per_node": 36}
             self.add_experiment_variable("n_threads_per_proc", 1, True)
+
+        if self.spec.satisfies("+single_node"):
+            for pk, pv in n_resources.items():
+                self.add_experiment_variable(pk, pv, True)
+        elif self.spec.satisfies("+strong"):
+            scaled_variables = self.generate_strong_scaling_params(
+                {tuple(n_resources.keys()): list(n_resources.values())},
+                int(self.spec.variants["scaling-factor"][0]),
+                int(self.spec.variants["scaling-iterations"][0]),
+            )
+            for pk, pv in scaled_variables.items():
+                self.add_experiment_variable(pk, pv, True)
 
         self.add_experiment_variable("timesteps", 100, False)
         self.add_experiment_variable("input_file", "{input_path}/in.reaxc.hns", False)
@@ -90,6 +98,11 @@ class Lammps(
             f"{input_sizes} -k on {kokkos_mode} -sf kk -pk kokkos gpu/aware {kokkos_gpu_aware} neigh half comm {kokkos_comm} neigh/qeq full newton on -nocite",
             False,
         )
+
+        self.add_experiment_variable("n_resources", "{n_nodes}*{n_ranks_per_node}", False)
+        self.add_experiment_variable("process_problem_size", "{xx}*{yy}*{zz}/{n_resources}", False)
+        self.add_experiment_variable("total_problem_size", "{xx}*{yy}*{zz}", False)
+
 
     def compute_package_section(self):
         # get package version
