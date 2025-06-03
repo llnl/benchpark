e733d8b039c1774f2f7fed2ac49a58e925374df5
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-06-02 17:20:14 -0700

    Increase color palette

--- lib/benchpark/cmd/analyze.py
+++ lib/benchpark/cmd/analyze.py
@@ -16,16 +16,26 @@ import thicket as th
 # Constants
 # -----------------------------
 COLOR_PALETTE = [
-    "#00FFFF",
-    "#ff7f00",
-    "#4daf4a",
-    "#f781bf",
-    "#a65628",
-    "#984ea3",
-    "#999999",
-    "#e41a1c",
-    "#dede00",
-    "#377eb8",
+    "#1f77b4",  # Blue
+    "#ff7f0e",  # Orange
+    "#2ca02c",  # Green
+    "#d62728",  # Red
+    "#9467bd",  # Purple
+    "#8c564b",  # Brown
+    "#e377c2",  # Pink
+    "#7f7f7f",  # Gray
+    "#bcbd22",  # Olive
+    "#17becf",  # Cyan
+    "#aec7e8",  # Light Blue
+    "#ffbb78",  # Light Orange
+    "#98df8a",  # Light Green
+    "#ff9896",  # Light Red
+    "#c5b0d5",  # Light Purple
+    "#c49c94",  # Light Brown
+    "#f7b6d2",  # Light Pink
+    "#c7c7c7",  # Light Gray
+    "#dbdb8d",  # Light Olive
+    "#9edae5"   # Light Cyan
 ]
 SCALING_TYPES = ["+strong", "+throughput", "+weak"]
 NAME_REMAP = {
