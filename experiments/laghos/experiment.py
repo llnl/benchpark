f08240d9510f42fdb3aad2a519ee31c02734f158
Riyaz Haque <your-email@example.com>
2025-06-04 16:42:56 -0700

    Fix laghos experiment.py

--- experiments/laghos/experiment.py
+++ experiments/laghos/experiment.py
@@ -36,32 +36,26 @@ class Laghos(
 
     maintainers("wdhawkins")
 
-    @requires_experiment_variables("device_scaling_factor")
+    @requires_experiment_variables("scaling_factor")
     def strong_scale(self):
-        device_scaling_factor = self.expr_vars.device_scaling_factor
+        scaling_factor = self.expr_vars.scaling_factor
 
         num_exprs = int(self.spec.variants["scaling-iterations"][0]) - 1
-        scaling_factor = int(self.spec.variants["scaling-factor"][0])
+        var_scaling_factor = int(self.spec.variants["scaling-factor"][0])
 
-        start_dim = device_scaling_factor.min_dim
-        ndims = device_scaling_factor.ndims
+        start_dim = scaling_factor.min_dim
+        ndims = scaling_factor.ndims
 
         for itr in range(num_exprs):
             dim = (start_dim + itr) % ndims
-            device_scaling_factor.scale_dim(dim, lambda v: v * scaling_factor)
+            scaling_factor.scale_dim(dim, lambda v: v * var_scaling_factor)
 
-        return None
+        return []
 
     def register_required_variables(self):
-        self.required_vars.extend(["nprocs", "process_problem_size", "total_problem_size"])
+        self.required_vars.extend(["n_resources", "process_problem_size", "total_problem_size"])
 
     def compute_applications_section(self):
-        # "zones" defined from mesh file, we are hardcoding it here
-        problem_sizes = {"zones": 1024}
-
-        for nk, nv in problem_sizes.items():
-            self.add_scalar_variable(nk, nv, True)
-
         if self.spec.satisfies("+cuda") or self.spec.satisfies("+rocm"):
             device = "n_gpus"
             n_devices_per_node = "{sys_gpus_per_node}"
@@ -70,21 +64,27 @@ class Laghos(
             n_devices_per_node = "{sys_cores_per_node}"
             self.add_experiment_variable("n_threads_per_proc", 1)
 
-        self.add_scalar_variable("n_resources", f"{{device}}")
+        self.add_scalar_variable(
+            device, f"{n_devices_per_node} * {{scaling_factor}}", True
+        )
+
+        self.add_scalar_variable("n_resources", f"{{{device}}}")
         self.add_scalar_variable("process_problem_size", "{zones} / {n_resources}")
         self.add_scalar_variable("total_problem_size", "{zones}")
 
     def initialize_experiment_variables(self):
         # The total number of resources for this experiment is calculated as:
-        # n_devices = n_devices_per_node * device_scaling_factor
-        # Scaling (strong) is achieved by scaling the device_scaling_factor variable
+        # n_devices = n_devices_per_node * scaling_factor
+        # Scaling (strong) is achieved by scaling the scaling_factor variable
         # For mpi-only builds:
         # n_devices_per_node = sys_cores_per_node, by default
         # n_devices = n_ranks
         # For gpu builds:
         # n_devices_per_node = sys_gpus_per_node, by default
         # n_devices = n_gpus
-        self.add_scalar_variable("device_scaling_factor", 1, named=True, scalable=True)
+        # "zones" defined from mesh file, we are hardcoding it here
+        self.add_scalar_variable("zones", 1024, named=True)
+        self.add_scalar_variable("scaling_factor", 1, named=False, scalable=True)
 
     def setup_default_experiment(self):
         pass
