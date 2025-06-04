0632e74d50aafb62cdab94d32d405de5c2bd321f
Ian Lumsden <lumsden.ian@gmail.com>
2025-06-04 16:17:11 -0700

    Adds an override to benchpark_home via the environment variable BENCHPARK_HOME

--- lib/benchpark/paths.py
+++ lib/benchpark/paths.py
@@ -16,7 +16,12 @@ def _source_location() -> pathlib.Path:
 benchpark_root = _source_location()
 lib_path = benchpark_root / "lib" / "benchpark"
 test_path = lib_path / "test"
-benchpark_home = pathlib.Path(os.path.expanduser("~/.benchpark"))
+
+try:
+    benchpark_home = os.environ["BENCHPARK_HOME"]
+except KeyError:
+    benchpark_home = pathlib.Path(os.path.expanduser("~/.benchpark"))
+
 global_ramble_path = benchpark_home / "ramble"
 global_spack_path = benchpark_home / "spack"
 hardware_descriptions = benchpark_root / "systems" / "all_hardware_descriptions"
