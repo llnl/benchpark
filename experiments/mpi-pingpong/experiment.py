ff29022678aa6eb63385099b579cebfebabd31d5
Stephanie Christine Lam <lam44@ruby967.llnl.gov>
2025-06-09 15:16:42 -0700

    loops through message sizes and also gets multiple cali files with 1 experiment

--- experiments/mpi-pingpong/experiment.py
+++ experiments/mpi-pingpong/experiment.py
@@ -6,10 +6,10 @@
 from benchpark.directives import variant, maintainers
 from benchpark.experiment import Experiment
 from benchpark.caliper import Caliper
-from benchpark.affinity import Affinity
+#from benchpark.affinity import Affinity
 
 
-class MpiPingpong(Experiment, Caliper, Affinity):
+class MpiPingpong(Experiment, Caliper):
 
     variant(
         "workload",
@@ -19,21 +19,32 @@ class MpiPingpong(Experiment, Caliper, Affinity):
     maintainers("stephanielam3211")
 
     def compute_applications_section(self):
-        n_ranks = 448   #dane:896, ruby:448, tioga:512, lassen:352, tuolumne:768
+        n_ranks = 1   #dane:896, ruby:448, tioga:512, lassen:352, tuolumne:768
+        n_nodes = 8     #max number of nodes
         expr_vars = {
-            "n_ranks": [n_ranks]*5, #[113]*5,
-            #"n_nodes" : 2,
+            "n_ranks": n_ranks, #[113]*5,
+            "n_nodes": n_nodes,
             #"iterations": [10]*5+[10000]*5+[100000]*5,
             #"msg_size": [16,256,4096,65536,1048576]*3
             # TODO: other expr vars?
-            "iterations" : [1000000]*5, #1000000,
-            "msg_size" : [16384, 32768, 65536, 131072, 262144]*1      #[16,256,4096,65536,1048576]*1
+            "iterations" : 1000000, #1000000,
+            "msg_size" : 16384     #[16,256,4096,65536,1048576]*1
         }
 
         for pk, pv in expr_vars.items():
             self.add_experiment_variable(pk, pv, True)
 
-        self.add_experiment_variable("partner_rank", n_ranks-1)
+        self.add_experiment_variable(
+            "n_ranks", "{n_nodes}*{sys_cores_per_node}", True
+        )
+
+        self.add_experiment_variable("partner_rank", "{n_ranks}-1", True)
+
+        self.set_required_variables(
+            n_resources="{n_ranks}",
+            process_problem_size="{msg_size}",
+            total_problem_size="{msg_size}",
+        )
 
     def compute_package_section(self):
         self.add_package_spec(self.name, ["mpi-pingpong@main"])
