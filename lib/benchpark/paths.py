4285777debbeb9b294ef4ba5b54753becb16de4b
pearce8 <pearce8@llnl.gov>
2025-06-10 04:30:32 -0700

    lint

--- lib/benchpark/paths.py
+++ lib/benchpark/paths.py
@@ -17,7 +17,9 @@ benchpark_root = _source_location()
 lib_path = benchpark_root / "lib" / "benchpark"
 test_path = lib_path / "test"
 
-benchpark_home = pathlib.Path(os.getenv("BENCHPARK_HOME", default=os.path.expanduser("~/.benchpark")))
+benchpark_home = pathlib.Path(
+    os.getenv("BENCHPARK_HOME", default=os.path.expanduser("~/.benchpark"))
+)
 
 global_ramble_path = benchpark_home / "ramble"
 global_spack_path = benchpark_home / "spack"
