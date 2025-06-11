35c981cceaf2a3c52221a2e655c69cf65b79a331
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-06-11 14:13:03 -0700

    Fix

--- repo/raja-perf/application.py
+++ repo/raja-perf/application.py
@@ -20,7 +20,12 @@ class RajaPerf(ExecutableApplication):
 
     executable('run', 'raja-perf.exe --size {size}', use_mpi=True)
 
-    workload('suite', executables=['run'])
+    executable(
+        "modules",
+        "export LD_LIBRARY_PATH=/opt/cray/pe/cce/18.0.1/cce/x86_64/lib:$LD_LIBRARY_PATH",
+    )
+
+    workload('suite', executables=['modules', 'run'])
 
     figure_of_merit('All tests pass', log_file='{experiment_run_dir}/{experiment_name}.out', fom_regex=r'(?P<tpass>DONE)!!!...', group_name='tpass', units='')
 
