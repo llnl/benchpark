891dadd135577d64f509883ae92ab7352ab92151
Amr Akmal Moustafa Abouelmagd <abouelmagd1@dane4.llnl.gov>
2025-06-18 01:35:38 -0700

    Added throughput scaling Co-authored-by: michaelmckinsey1

--- experiments/raja-perf/experiment.py
+++ experiments/raja-perf/experiment.py
@@ -10,6 +10,7 @@ from benchpark.scaling import StrongScaling
 from benchpark.openmp import OpenMPExperiment
 from benchpark.cuda import CudaExperiment
 from benchpark.rocm import ROCmExperiment
+from benchpark.scaling import ThroughputScaling
 from benchpark.caliper import Caliper
 
 
@@ -19,6 +20,7 @@ class RajaPerf(
     CudaExperiment,
     ROCmExperiment,
     OpenMPExperiment,
+    ThroughputScaling,
     Caliper,
 ):
     variant(
@@ -87,6 +89,15 @@ class RajaPerf(
             )
             problem_sizes = scaled_problem_sizes["size"]
             for nk, nv in scaled_problem_sizes.items():
+                self.add_experiment_variable(nk, nv, True)
+        elif self.spec.satisfies("+throughput"):
+            scaled_variables = self.generate_throughput_scaling_params(
+                {tuple(problem_sizes.keys()): list(problem_sizes.values())},
+                int(self.spec.variants["scaling-factor"][0]),
+                int(self.spec.variants["scaling-iterations"][0]),
+            )
+            n_resources = n_resources["n_ranks"]
+            for nk, nv in scaled_variables.items():
                 self.add_experiment_variable(nk, nv, True)
 
         if self.spec.satisfies("+cuda"):
