20f99cc23b2d06b27161da69bc6b6dce15084a2a
Peter Josef Scheibel <scheibel1@llnl.gov>
2025-06-27 17:06:30 -0700

    completed script for flux: generates 1 grouped script for each unique batch request (so e.g. all 1-node requests will go into one script)

--- aggregate.py
+++ aggregate.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import os
 import re
 
@@ -16,7 +17,49 @@ def main():
     for workspace_dir in args.workspaces:
         experiments.extend(collect_experiments(workspace_dir))
 
-    import pdb; pdb.set_trace()
+    opts_to_request = {}
+    opts_to_scripts = defaultdict(list)
+    for experiment_script in experiments:
+        batch_opts, batch_lines = collect_scheduler_options(experiment_script)
+        if batch_opts:
+            opts_to_scripts[batch_opts].append(experiment_script)
+            opts_to_request[batch_opts] = batch_lines
+        else:
+            #import pdb; pdb.set_trace()
+            raise Exception(f"Not expected: no batch opts in {experiment_script}")
+
+    os.mkdir(args.output)
+    script_id = 0
+    for opts_group, scripts in opts_to_scripts.items():
+        script_path = os.path.join(args.output, f"{script_id}.sh")
+        make_aggregate_script(script_path, opts_to_request[opts_group], scripts)
+        script_id += 1
+
+def make_aggregate_script(script_path, batch_lines, to_aggregate):
+    with open(script_path, "w") as f:
+        for line in batch_lines:
+            f.write(line + "\n")
+        for experiment in to_aggregate:
+            f.write(os.path.abspath(experiment) + "\n")
+
+def collect_scheduler_options(experiment_script):
+    batch_patterns = [
+        r"\s*#\s*(flux:.*$)"
+    ]
+    batch_opts = list()
+    batch_lines = list()
+    with open(experiment_script, "r") as f:
+        for line in f.readlines():
+            for p in batch_patterns:
+                m = re.match(p, line)
+                if m:
+                    batch_opts.append(tuple(m.group(1).strip().split()))
+                    batch_lines.append(line.strip())
+                #elif "flux" in line:
+                #    import pdb; pdb.set_trace()
+                #    print('hi')
+
+    return tuple(sorted(batch_opts)), batch_lines
 
 def collect_experiments(workspace_dir):
     experiments = list()
