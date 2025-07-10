3aa3afc3fd93625f4df0eeb5f7a7c83707e9f575
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-07-10 13:16:56 -0700

    Remove builds dir

--- .gitlab/utils/cancel-flux.sh
+++ .gitlab/utils/cancel-flux.sh
@@ -2,3 +2,5 @@
 set -x
 export URI=$(flux jobs -o "{id} {name}" | grep ${ALLOC_NAME}${GPUMODE} | awk '{print $1}')
 ([[ -n "${URI}" ]] && flux job kill ${URI} || exit 0)
+echo "Removing $CUSTOM_CI_BUILDS_DIR"
+rm -rf $CUSTOM_CI_BUILDS_DIR
--- .gitlab/utils/cancel-slurm.sh
+++ .gitlab/utils/cancel-slurm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -x
 export JOBID=$(squeue -h --name=${ALLOC_NAME} --format=%A)
 ([[ -n "${JOBID}" ]] && scancel ${JOBID} || exit 0)
+echo "Removing $CUSTOM_CI_BUILDS_DIR"
+rm -rf $CUSTOM_CI_BUILDS_DIR
