c9b76b9157cb06bfbc278a36d6fa41a8354b7bd2
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-07-17 15:54:19 -0700

    Update list to take programming model as optional arg

--- lib/benchpark/cudasystem.py
+++ lib/benchpark/cudasystem.py
@@ -9,6 +9,8 @@ from benchpark.directives import provides
 class CudaSystem:
     provides("cuda")
 
+    name = "cuda"
+
     def verify(self, system):
         assert "cuda" in system.variants
         assert "gtl" in system.variants
