31f405bf1034276c5bdc36502184a5c2163520ed
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-07-18 14:05:03 -0700

    Fix path

--- .github/utils/dryruns.py
+++ .github/utils/dryruns.py
@@ -114,7 +114,7 @@ def main():
                 ran_tests += 1
                 print(f"Running '{espec}' '{sspec}'")
                 try:
-                    cmd = f'source .github/workflows/utils/dryrun.sh "{sspec}" "{espec}"'
+                    cmd = f'source .github/utils/dryrun.sh "{sspec}" "{espec}"'
                     result = subprocess.run(
                         ["bash", "-c", cmd],
                         capture_output=True,
