690b61ddd227d9e35d0bfd28a87b0642d7f58212
Riyaz Haque <your-email@example.com>
2025-07-20 17:11:31 -0700

    babelstream experiment changes

--- experiments/babelstream/experiment.py
+++ experiments/babelstream/experiment.py
@@ -34,10 +34,9 @@ class Babelstream(
     maintainers("daboehme")
 
     def compute_applications_section(self):
+        self.add_experiment_variable("n", "50", True)
+        self.add_experiment_variable("s", "10240000", True)
 
-        self.add_experiment_variable("processes_per_node", "1", True)
-        self.add_experiment_variable("n", "35", False)
-        self.add_experiment_variable("o", "0", False)
         n_resources = 1
 
         if self.spec.satisfies("+cuda"):
@@ -47,22 +46,23 @@ class Babelstream(
             self.add_experiment_variable("execute", "hip-stream", False)
 
         else:
-            self.add_experiment_variable("n_ranks", n_resources, True)
             self.add_experiment_variable("execute", "omp-stream", False)
 
+        if self.spec.satisfies("+openmp"):
+            self.add_experiment_variable("n_threads_per_proc", 16, True)
         if self.spec.satisfies("+cuda") or self.spec.satisfies("+rocm"):
-            self.add_experiment_variable("n_gpus", n_resources, True)
+            self.add_experiment_variable("n_gpus", "{n_resources}", True)
+        else:
+            self.add_experiment_variable("n_ranks", "{n_resources}", True)
 
         self.set_required_variables(
             n_resources=f"{n_resources}",
-            process_problem_size="{n}/" + str(n_resources),
-            total_problem_size="{n}",
+            process_problem_size="{s}",
+            total_problem_size="{s} * {n_resources}",
         )
 
     def compute_package_section(self):
         # get package version
         app_version = self.spec.variants["version"][0]
-        omp_spec = ""
-        if self.spec.satisfies("+openmp"):
-            omp_spec = "+omp"
+        omp_spec = "+omp" if self.spec.satisfies("+openmp") else ""
         self.add_package_spec(self.name, [f"babelstream@{app_version} {omp_spec}"])
