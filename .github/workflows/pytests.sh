ac421b3589dc48ebe59af91026415c6554999241
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-07-28 18:41:32 -0700

    Fix

--- .github/workflows/pytests.sh
+++ .github/workflows/pytests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 for file in lib/benchpark/test/*.py; do
+    filename="$(basename "$file")"
+    if [[ "$filename" == "__init__.py" ]] || [[ "$filename" == "conftest.py" ]]; then
+        continue
+    fi
     echo "Running tests in $file"
     ./bin/benchpark unit-test "$file"
 done
