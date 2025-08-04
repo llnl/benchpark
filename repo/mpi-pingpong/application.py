e0487324e6e0035c42260bbf45f2ea53aa82b36d
Stephanie Christine Lam <lam44@ruby963.llnl.gov>
2025-08-04 09:51:19 -0700

    fixed application.py to match source code

--- repo/mpi-pingpong/application.py
+++ repo/mpi-pingpong/application.py
@@ -14,7 +14,7 @@ class MpiPingpong(ExecutableApplication):
     tags = ['single-node', 'builtin-caliper']
 
 
-    executable("pingpong", "pingpong -i {iterations} -m {msg_size} -p 0,{partner_rank} -n {experiment_run_dir}/{experiment_name}_metadata.json", use_mpi=True)
+    executable("pingpong", "pingpong -i {iterations} -m {msg_size} -p 0,{partner_rank} -n {n_nodes} -s {sys_cores_per_node}//{sys_sockets_per_node} -c {sys_cores_per_node} -b {experiment_run_dir}/{experiment_name}_metadata.json", use_mpi=True)
     #executable("pingpong1", "pingpong -i {iterations} -m {msg_size} -p 0,56", use_mpi=True)
     #executable("pingpong2", "pingpong -i {iterations} -m {msg_size} -p 0,112", use_mpi=True)
 
