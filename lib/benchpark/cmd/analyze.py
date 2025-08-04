24c689838867a604df147239667e2737bd43cabc
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-08-04 16:08:29 -0700

    Add features

--- lib/benchpark/cmd/analyze.py
+++ lib/benchpark/cmd/analyze.py
@@ -216,17 +216,7 @@ def prepare_data(**kwargs):
     )
     tk.update_inclusive_columns()
 
-    # Save tree before modification
-    # Cleans ANSI escape sequences and legend from a raw calltree string.
-    tk.dataframe["nothing"] = 0
-    raw_tree = tk.tree("nothing", render_header=False, precision=0)
-    ansi_escape = re.compile(r"\x1b\[([0-9;]*m)")
-    text = ansi_escape.sub("", raw_tree)
-    legend_index = text.find("Legend")
-    if legend_index != -1:
-        text = text[:legend_index]
-    clean_tree = text.replace("0", "")
-    kwargs["tree_str"] = clean_tree
+    clean_tree = tk.tree(kwargs["tree_metric"], render_header=True)
 
     # Remove MPI regions, if necesasry
     if kwargs.get("no_mpi"):
@@ -343,9 +333,12 @@ def prepare_data(**kwargs):
             f"{app}+{programming_model}@{version} on {cluster} ({scaling} scaling)\n{constant_str}"
         )
 
-    kwargs["chart_file_name"] = (
-        f"{app}_{programming_model}_{scaling}_{kwargs['chart_type']}_{'inc' if metric in tk.inc_metrics else 'exc'}"
-    )
+    if kwargs["output_filename"]:
+        kwargs["chart_file_name"] = kwargs["output_filename"]
+    else:
+        kwargs["chart_file_name"] = (
+            f"{app}_{programming_model}_{scaling}_{kwargs['chart_type']}_{'inc' if metric in tk.inc_metrics else 'exc'}"
+        )
 
     # Save tree to file
     tree_file = os.path.join(kwargs["out_dir"], kwargs["chart_file_name"] + "-tree.txt")
@@ -488,6 +481,18 @@ def setup_parser(root_parser):
         type=str,
         default="",
         help="Set optional cali file name to match. Useful if multiple caliper files are generated per experiment (e.g. RAJAPerf)",
+    )
+    root_parser.add_argument(
+        "--output-filename",
+        type=str,
+        default=None,
+        help="Configure the output file names (the defaul value is already unique to the workspace).",
+    )
+    root_parser.add_argument(
+        "--tree-metric",
+        type=str,
+        default="Calls/rank (max)",
+        help="Metric to show on the tree output",
     )
 
 
