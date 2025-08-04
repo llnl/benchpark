ddcac9ce558dd59ecf8068c315250be41d68f2ba
Stephanie Christine Lam <lam44@ruby963.llnl.gov>
2025-08-04 09:55:31 -0700

    fixing executable so it matches source code

--- repo/mpi-pingpong/application.py
+++ repo/mpi-pingpong/application.py
@@ -13,12 +13,8 @@ class MpiPingpong(ExecutableApplication):
 
     tags = ['single-node', 'builtin-caliper']
 
-<<<<<<< HEAD
 
     executable("pingpong", "pingpong -i {iterations} -m {msg_size} -p 0,{partner_rank} -n {n_nodes} -s {sys_cores_per_node}//{sys_sockets_per_node} -c {sys_cores_per_node} -b {experiment_run_dir}/{experiment_name}_metadata.json", use_mpi=True)
-=======
-    executable("pingpong", "pingpong -i {iterations} -m {msg_size} -p 0,{partner_rank} -n {experiment_run_dir}/{experiment_name}_metadata.json", use_mpi=True)
->>>>>>> e14db108c3e1d97127fda11e66c3f9a4c88c85c6
     #executable("pingpong1", "pingpong -i {iterations} -m {msg_size} -p 0,56", use_mpi=True)
     #executable("pingpong2", "pingpong -i {iterations} -m {msg_size} -p 0,112", use_mpi=True)
 
