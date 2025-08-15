e7937875d8ae4296e5cc456621b2977c9103ced7
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-08-15 11:58:29 -0700

    Add develop version tests

--- .gitlab/utils/run-experiment.sh
+++ .gitlab/utils/run-experiment.sh
@@ -12,7 +12,11 @@ else
 fi
 
 # Initialize Experiment
-./bin/benchpark experiment init --dest=${BENCHMARK} --system=${HOST} ${BENCHMARK} ${VARIANT}
+BV=""
+if [[ -n "$BENCHMARK_VERSION" ]]; then
+    BV="version=$BENCHMARK_VERSION"
+fi
+./bin/benchpark experiment init --dest=${BENCHMARK} --system=${HOST} ${BENCHMARK} ${VARIANT} ${BV}
 
 # Build Workspace
 ./bin/benchpark setup ${BENCHMARK} ${HOST} wkp/
