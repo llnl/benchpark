0b48651046645dfc7cddfefa7fd6a098447166a2
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-09-04 10:42:22 -0700

    Update experiment

--- experiments/scaffold/experiment.py
+++ experiments/scaffold/experiment.py
@@ -6,15 +6,17 @@
 from benchpark.error import BenchparkError
 from benchpark.directives import variant, maintainers
 from benchpark.experiment import Experiment
-from benchpark.scaling import StrongScaling
+from benchpark.cuda import CudaExperiment
 from benchpark.rocm import ROCmExperiment
+from benchpark.scaling import ScalingMode, Scaling
 from benchpark.caliper import Caliper
 
 
 class Scaffold(
     Experiment,
-    StrongScaling,
+    CudaExperiment,
     ROCmExperiment,
+    Scaling(ScalingMode.Strong),
     Caliper,
 ):
 
@@ -35,44 +37,28 @@ class Scaffold(
     variant("version", default="develop", description="app version")
 
     def compute_applications_section(self):
-        # TODO: Replace with conflicts clause
-        scaling_modes = {
-            "strong": self.spec.satisfies("+strong"),
-            "weak": self.spec.satisfies("+weak"),
-            "throughput": self.spec.satisfies("+throughput"),
-            "single_node": self.spec.satisfies("+single_node"),
-        }
-
-        scaling_mode_enabled = [key for key, value in scaling_modes.items() if value]
-        if len(scaling_mode_enabled) != 1:
-            raise BenchparkError(
-                f"Only one type of scaling per experiment is allowed for application package {self.name}"
-            )
-
         self.add_experiment_variable(
             "package_path", self.spec.variants["scaffold_path"][0], False
         )
         self.add_experiment_variable("timeout", 720, True)
 
-        problem_sizes = {"problem_scale": 6}
+        self.add_experiment_variable("n_gpus", 4, True)
 
-        for nk, nv in problem_sizes.items():
-            self.add_experiment_variable(nk, nv, True)
+        self.add_experiment_variable("problem_scale", 6, True)
 
-        if self.spec.satisfies("+rocm"):
-            n_resources = {"n_gpus": 4}
-
-        if self.spec.satisfies("+single_node"):
-            for pk, pv in n_resources.items():
-                self.add_experiment_variable(pk, pv, True)
-        elif self.spec.satisfies("+strong"):
-            scaled_variables = self.generate_strong_scaling_params(
-                {tuple(n_resources.keys()): list(n_resources.values())},
-                int(self.spec.variants["scaling-factor"][0]),
-                int(self.spec.variants["scaling-iterations"][0]),
-            )
-            for pk, pv in scaled_variables.items():
-                self.add_experiment_variable(pk, pv, True)
+        self.register_scaling_config(
+            {
+                ScalingMode.Strong: {
+                    "n_gpus": lambda var, itr, dim, scaling_factor: var.val(
+                        dim
+                    )
+                    * scaling_factor,
+                    "problem_scale": lambda var, itr, dim, scaling_factor: var.val(
+                        dim
+                    ),
+                },
+            }
+        )
 
         self.set_required_variables(
             n_resources="{n_gpus}",
