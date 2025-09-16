3409e9c1c3e31a8f4f1b2400293a52be90f2c1a8
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-09-16 16:24:15 -0700

    improve test

--- lib/benchpark/test/commands.py
+++ lib/benchpark/test/commands.py
@@ -70,7 +70,21 @@ def test_tags():
 
 
 def test_info():
-    subprocess.run(
-        [benchpark.paths.benchpark_root / "bin/benchpark", "info", "system", "llnl-cluster"],
+    text = subprocess.run(
+        ["benchpark", "list", "systems", "--no-title"],
+        capture_output=True,
+        text=True,
         check=True,
     )
+    result = [
+        line.lstrip().split(" ")[0]
+        for line in text.stdout.splitlines()
+        if line.strip() and not line.lstrip().startswith("generic-x86")
+    ]
+
+    for r in result:
+        subprocess.run(
+            [benchpark.paths.benchpark_root / "bin/benchpark", "info", "system", r],
+            check=True,
+            capture_output=True,
+        )
