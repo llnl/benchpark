95292ab8ccd840f882cffd713582a2c0e9dbc87f
Peter Scheibel <scheibel1@llnl.gov>
2025-09-19 16:41:27 -0700

    point SPACK_USER_CACHE_PATH inside benchpark workspace

--- lib/benchpark/cmd/setup.py
+++ lib/benchpark/cmd/setup.py
@@ -172,6 +172,7 @@ def command(args):
     pkg_str = ""
     if pkg_manager == "spack":
         spack_build_stage = experiments_root / "builds"
+        spack_user_cache_path = experiments_root / "spack-cache"
         spack, first_time_spack = per_workspace_setup.spack_first_time_setup()
         if first_time_spack:
             site_repos = (
@@ -188,9 +189,9 @@ repos::
             spack(f'config --scope=site add "config:build_stage:[\'{spack_build_stage}\']"')
 
         pkg_str = f"""\
-. {per_workspace_setup.spack_location}/share/spack/setup-env.sh
-
+export SPACK_USER_CACHE_PATH={spack_user_cache_path}
 export SPACK_DISABLE_LOCAL_CONFIG=1
+. {per_workspace_setup.spack_location}/share/spack/setup-env.sh
 """
 
     ramble, first_time_ramble = per_workspace_setup.ramble_first_time_setup()
