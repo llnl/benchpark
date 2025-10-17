65b0a8f900e73ee62725fd0ebef51699cb52088e
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-10-17 09:34:50 -0700

    Fix default compiler

--- systems/llnl-elcapitan/system.py
+++ systems/llnl-elcapitan/system.py
@@ -749,7 +749,7 @@ class LlnlElcapitan(System):
         return {
             "software": {
                 "packages": {
-                    "default-compiler": {"pkg_spec": "gcc"},
+                    "default-compiler": {"pkg_spec": default_compiler},
                     "default-mpi": {"pkg_spec": "cray-mpich"},
                     "compiler-rocm": {"pkg_spec": "cce"},
                     "compiler-amdclang": {"pkg_spec": "clang"},
