5d50d73b63d0fee171f0a9942e40a0c34a06f28f
pearce8 <pearce8@llnl.gov>
2025-10-27 15:22:08 -0500

    Commbench experiment license

--- experiments/commbench/experiment.py
+++ experiments/commbench/experiment.py
@@ -1,3 +1,8 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from benchpark.directives import variant, maintainers
 from benchpark.experiment import Experiment
 from benchpark.openmp import OpenMPExperiment
