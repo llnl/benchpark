85f16c7e53dd7f19947d9ed38cbf1d5d60e6b33d
pearce8 <pearce8@llnl.gov>
2025-10-27 19:01:27 -0500

    lint imports

--- experiments/commbench/experiment.py
+++ experiments/commbench/experiment.py
@@ -3,10 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from benchpark.directives import variant, maintainers
+from benchpark.cuda import CudaExperiment
+from benchpark.directives import maintainers, variant 
 from benchpark.experiment import Experiment
 from benchpark.openmp import OpenMPExperiment
-from benchpark.cuda import CudaExperiment
 from benchpark.rocm import ROCmExperiment
 
 
