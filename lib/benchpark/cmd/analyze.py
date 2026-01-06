249b015f0dda4d04221325f6d6f32cfe8e7a83d1
Michael McKinsey <MichaelMcKinsey1@gmail.com>
2026-01-06 13:47:31 -0800

    Update analyze.py

--- lib/benchpark/cmd/analyze.py
+++ lib/benchpark/cmd/analyze.py
@@ -409,7 +409,9 @@ def prepare_data(**kwargs):
             "throughput": ["total_problem_size"],
         }[scaling]
     )
-    kwargs["xaxis_parameter"] = [x_axis_metadata] if not isinstance(x_axis_metadata, list) else x_axis_metadata
+    kwargs["xaxis_parameter"] = (
+        [x_axis_metadata] if not isinstance(x_axis_metadata, list) else x_axis_metadata
+    )
 
     if kwargs.get("group_regions_name"):
         logger.info(
