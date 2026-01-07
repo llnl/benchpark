1bab708ed469810481e312585033d28dbf5fc13c
Michael McKinsey <MichaelMcKinsey1@gmail.com>
2026-01-07 13:07:24 -0800

    Return consistent type in `benchpark analyze` (#1180)
    * Update analyze.py
    
    * Update analyze.py
    
    * Do group after query
    
    ---------
    
    Co-authored-by: pearce8 <pearce8@llnl.gov>

--- lib/benchpark/cmd/analyze.py
+++ lib/benchpark/cmd/analyze.py
@@ -422,10 +422,28 @@ def prepare_data(**kwargs):
         or {
             "strong": ["n_nodes", "n_resources"],
             "weak": ["n_nodes", "n_resources", "total_problem_size"],
-            "throughput": "total_problem_size",
+            "throughput": ["total_problem_size"],
         }[scaling]
     )
-    kwargs["xaxis_parameter"] = x_axis_metadata
+    kwargs["xaxis_parameter"] = (
+        [x_axis_metadata] if not isinstance(x_axis_metadata, list) else x_axis_metadata
+    )
+
+    region_names = kwargs.get("query_regions_byname", "")
+    if region_names:
+        query = (
+            th.query.Query()
+            .match(
+                ".", lambda row: row["name"].apply(lambda n: n in region_names).all()
+            )
+            .rel("*")
+        )
+
+        tk = tk.query(query)
+
+    prefix = kwargs.get("filter_regions_byname", "")
+    if prefix:
+        tk.dataframe = pd.concat([tk.dataframe.filter(like=p, axis=0) for p in prefix])
 
     if kwargs.get("group_regions_name"):
         logger.info(
@@ -448,22 +466,6 @@ def prepare_data(**kwargs):
         )
         tk.dataframe = grouped
         tk = tk.squash()
-
-    region_names = kwargs.get("query_regions_byname", "")
-    if region_names:
-        query = (
-            th.query.Query()
-            .match(
-                ".", lambda row: row["name"].apply(lambda n: n in region_names).all()
-            )
-            .rel("*")
-        )
-
-        tk = tk.query(query)
-
-    prefix = kwargs.get("filter_regions_byname", "")
-    if prefix:
-        tk.dataframe = pd.concat([tk.dataframe.filter(like=p, axis=0) for p in prefix])
 
     cluster_col = "cluster" if "cluster" in tk.metadata.columns else "host.cluster"
     tk.metadata_columns_to_perfdata([cluster_col] + list(NAME_REMAP.keys()))
