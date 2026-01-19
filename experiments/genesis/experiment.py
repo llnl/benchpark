49dc98986a178f5487485d32f46138d98772e023
pearce8 <pearce8@llnl.gov>
2026-01-18 19:57:34 -0600

    lint

--- experiments/genesis/experiment.py
+++ experiments/genesis/experiment.py
@@ -11,10 +11,10 @@ from benchpark.scaling import Scaling, ScalingMode
 
 
 class Genesis(
-  Experiment,
-  MpiOnlyExperiment,
-  OpenMPExperiment,
-  Scaling(ScalingMode.Strong, ScalingMode.Weak, ScalingMode.Throughput),
+    Experiment,
+    MpiOnlyExperiment,
+    OpenMPExperiment,
+    Scaling(ScalingMode.Strong, ScalingMode.Weak, ScalingMode.Throughput),
 ):
 
     variant(
@@ -47,7 +47,7 @@ class Genesis(
             )
             self.add_experiment_variable("lt", "3")
             self.add_experiment_variable(
-              "n_resources_dict", {"px": 1, "py": 1, "pz": 1}, True
+                "n_resources_dict", {"px": 1, "py": 1, "pz": 1}, True
             )
             self.add_experiment_variable("pt", "1")
         # Must be exec_mode=perf
