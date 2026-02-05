e799729c33d7d056c406fd57f6e3e98095dde43f
Michael McKinsey <michaelmckinsey1@gmail.com>
2026-02-05 15:00:54 -0800

    lint

--- repos/spack_repo/benchpark/packages/py_scaffold/package.py
+++ repos/spack_repo/benchpark/packages/py_scaffold/package.py
@@ -5,11 +5,10 @@
 
 import os
 
+from spack.package import *
 from spack_repo.builtin.build_systems.cuda import CudaPackage
 from spack_repo.builtin.build_systems.python import PythonPackage
 from spack_repo.builtin.build_systems.rocm import ROCmPackage
-
-from spack.package import *
 
 
 class PyScaffold(PythonPackage, CudaPackage, ROCmPackage):
