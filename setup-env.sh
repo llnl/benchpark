9dfb0bf20e291998a0690be15e3689e507ca226f
Peter Scheibel <scheibel1@llnl.gov>
2026-02-12 14:57:27 -0800

    setup script edits

--- setup-env.sh
+++ setup-env.sh
@@ -1,1 +1,9 @@
-export PATH=${PWD}/bin:$PATH
+_this_file="${BASH_SOURCE[0]-${(%):-%N}-$0}"
+_this_dir="$(CDPATH= cd -- "$(dirname -- "$_this_file")" 2>/dev/null && pwd -P)"
+
+if [ -n "$PATH" ] && [ ":$PATH:" != "${PATH#:$_this_dir/bin:}" ]; then
+  :  # already present
+else
+  PATH="$_this_dir/bin${PATH+:$PATH}"
+  export PATH
+fi
