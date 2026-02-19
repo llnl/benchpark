9ae69140303ea87d3f39cb534a9d2d7f186ce529
Michael McKinsey <michaelmckinsey1@gmail.com>
2026-02-19 11:38:14 -0800

    lint

--- experiments/py-scaffold/experiment.py
+++ experiments/py-scaffold/experiment.py
@@ -31,9 +31,17 @@ class PyScaffold(
 
     def compute_applications_section(self):
         if self.spec.satisfies("exec_mode=test"):
-            self.add_experiment_variable("config_url", "https://raw.githubusercontent.com/LBANN/ScaFFold/refs/heads/main/ScaFFold/configs/benchmark_testing.yml", False)
+            self.add_experiment_variable(
+                "config_url",
+                "https://raw.githubusercontent.com/LBANN/ScaFFold/refs/heads/main/ScaFFold/configs/benchmark_testing.yml",
+                False,
+            )
         else:
-            self.add_experiment_variable("config_url", "https://raw.githubusercontent.com/LBANN/ScaFFold/refs/heads/main/ScaFFold/configs/benchmark_default.yml", False)
+            self.add_experiment_variable(
+                "config_url",
+                "https://raw.githubusercontent.com/LBANN/ScaFFold/refs/heads/main/ScaFFold/configs/benchmark_default.yml",
+                False,
+            )
 
         if self.spec.satisfies("+strong"):
             n_gpus = 4
