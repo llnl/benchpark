297163838b93b9b4b6848ca9e87cc15a351bc54c
Peter Josef Scheibel <scheibel1@llnl.gov>
2026-02-26 23:46:55 -0800

    inserting rpaths kind-of replicates the logic of the spack compiler wrapper; this is testing something of a hack to make use of it to do all the rpath insertion we want, but into an invocation of the hip compiler

--- repos/spack_repo/benchpark/packages/hip_wrapper/package.py
+++ repos/spack_repo/benchpark/packages/hip_wrapper/package.py
@@ -19,43 +19,17 @@ class HipWrapper(BundlePackage):
 
     depends_on("llvm-amdgpu")
 
-    def setup_dependent_build_environment(self, env, dependent_spec):
-        rpaths = []
-        if dependent_spec.package.compiler.extra_rpaths:
-            for rpath in dependent_spec.package.compiler.extra_rpaths:
-                rpaths.append(f"-Wl,-rpath,{rpath}")
-        env.set("SPACK_HIP_WRAPPER_LIBS", " ".join(rpaths))
-
-    #def dependent_cmake_args(self, dependent_spec: Spec) -> List[str]:
-    #    x = self.spec.prefix.bin.hipwrapper
-    #    return [
-    #        f"-DCMAKE_HIP_COMPILER_LAUNCHER={x}",
-    #        #f"-DCMAKE_HIP_LINKER_LAUNCHER={x}",
-    #        f"-DCMAKE_RULE_LAUNCH_LINK={x}",
-    #    ]
-
     def install(self, spec, prefix):
         mkdir(self.prefix.bin)
         fpath = os.path.join(self.prefix.bin, "hipwrapper")
         hip_compiler = os.path.join(spec["llvm-amdgpu"].prefix.llvm.bin, "clang++")
+        # Usually spack sets CXX to be the spack compiler wrapper
         with open(fpath, "w") as f:
             f.write(
                 f"""\
 #!/bin/bash
 
-is_compile=0
-for arg in "$@"; do
-    if [[ "$arg" == "-c" ]]; then
-        is_compile=1
-        break
-    fi
-done
-
-if [[ $is_compile -eq 1 ]]; then
-    exec {hip_compiler} "$@"
-else
-    exec {hip_compiler} $SPACK_HIP_WRAPPER_LIBS "$@"
-fi
+SPACK_CXX={hip_compiler} $CXX "$@"
 """
                     )
         st = os.stat(fpath)
