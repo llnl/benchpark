9d33e629a357aeb79ce2a2fd92cc438e7d05ba29
Michael McKinsey <michaelmckinsey1@gmail.com>
2026-02-26 22:25:16 -0800

    Simplify checks since error will be caught by spec logic

--- lib/benchpark/experiment.py
+++ lib/benchpark/experiment.py
@@ -260,26 +260,17 @@ class Experiment(ExperimentSystemBase, ExecMode, Affinity, Hwloc):
 
         # Explicitly ordered list. "mpi" first
         models = ["mpi"] + ["openmp", "cuda", "rocm"]
-        valid_models = []
-        invalid_models = []
-        for model in models:
-            if self.spec.satisfies("+" + model):
-                valid_models.append(model)
-                # Experiment specifying model in add_package_spec that it doesn't implement
-                if model not in self.programming_models:
-                    invalid_models.append(model)
-        # MPI is always valid if with another programming model, even if no mpionly
-        if "mpi" in invalid_models and len(valid_models) > 1:
-            invalid_models.remove("mpi")
         # Case where there are no experiments specified in experiment.py
         if len(self.programming_models) == 0:
             raise BenchparkError(
                 f"Please specify a programming model in your {self.name}/experiment.py (e.g. ProgrammingModelType.Mpionly, ProgrammingModelType.Openmp, ProgrammingModelType.Cuda, ProgrammingModelType.Rocm). See other experiments for examples."
             )
-        elif len(invalid_models) > 0:
-            print(self.spec)
+        # Check if experiment is trying to run in MpiOnly mode without being an ProgrammingModelType.Mpionly experiment
+        elif "mpi" not in str(self.spec) and not any(
+            self.spec.satisfies("+" + model) for model in models[1:]
+        ):
             raise BenchparkError(
-                f'{invalid_models} are not valid programming models for "{self.name}". Choose from {self.programming_models}.'
+                f'"{self.name}" cannot run with MPI only without inheriting from ProgrammingModelType.Mpionly. Choose from {self.programming_models}'
             )
 
         if (
