e75098af89eb8ce3b7b1af000beb22bdbddcb7eb
Michael McKinsey <michaelmckinsey1@gmail.com>
2026-02-27 15:22:44 -0800

    lint

--- experiments/mpi-pingpong/experiment.py
+++ experiments/mpi-pingpong/experiment.py
@@ -3,16 +3,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from benchpark.directives import variant, maintainers
-from benchpark.experiment import Experiment
-from benchpark.mpi import MpiOnlyExperiment
 from benchpark.caliper import Caliper
-from benchpark.rocm import ROCmExperiment
-from benchpark.cuda import CudaExperiment
+from benchpark.directives import maintainers, variant
+from benchpark.experiment import Experiment
+from benchpark.programming_model import ProgrammingModel, ProgrammingModelType
 
 
 class MpiPingpong(
-    Experiment, MpiOnlyExperiment, ROCmExperiment, CudaExperiment, Caliper
+    Experiment,
+    ProgrammingModel(
+        ProgrammingModelType.Mpionly,
+        ProgrammingModelType.Rocm,
+        ProgrammingModelType.Cuda,
+    ),
+    Caliper,
 ):
 
     variant(
