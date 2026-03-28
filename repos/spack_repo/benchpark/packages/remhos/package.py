03f3579b40024c0ba57dfb867f53a678f1b5ec57
Riyaz Haque <haque1@llnl.gov>
2026-03-27 19:14:53 -0700

    Enable RAJA in mfem

--- repos/spack_repo/benchpark/packages/remhos/package.py
+++ repos/spack_repo/benchpark/packages/remhos/package.py
@@ -19,19 +19,19 @@ class Remhos(MakefilePackage, CudaPackage, ROCmPackage):
     tags = ["proxy-app"]
 
     homepage = "https://github.com/CEED/Remhos"
-    git = "https://github.com/CEED/Remhos.git"
+    git = "https://github.com/rfhaque/Remhos.git"
 
     maintainers("v-dobrev", "tzanio", "vladotomov")
 
     license("BSD-2-Clause")
 
     version("develop", branch="master")
-    version("gpu-fom", branch="gpu-fom")
     version("1.0", sha256="e60464a867fe5b1fd694fbb37bb51773723427f071c0ae26852a2804c08bbb32")
 
     variant("metis", default=True, description="Enable/disable METIS support")
     variant("caliper", default=False, description="Enable/disable Caliper support")
     variant("gpu-aware-mpi", default=False, description="Enable GPU aware MPI")
+    variant("raja", default=True, description="Use RAJA backend for MFEM")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
@@ -39,6 +39,7 @@ class Remhos(MakefilePackage, CudaPackage, ROCmPackage):
 
     depends_on("mfem+mpi+metis", when="+metis")
     depends_on("mfem+mpi~metis", when="~metis")
+    depends_on("mfem+raja", when="+raja")
     depends_on("caliper", when="+caliper")
     depends_on("adiak~shared", when="+caliper")
 
@@ -52,27 +53,29 @@ class Remhos(MakefilePackage, CudaPackage, ROCmPackage):
 
     depends_on("mpi")
     depends_on("hypre+mpi")
-    depends_on("hypre+cuda+mpi", when="+cuda")
     depends_on("hypre+mixedint~fortran")
     depends_on("hypre+caliper", when="+caliper")
 
-    requires("+cuda", when="^hypre+cuda")
-    for arch in ("none", "50", "60", "70", "80", "90"):
-        depends_on(f"hypre cuda_arch={arch}", when=f"cuda_arch={arch}")
-        depends_on(f"mfem cuda_arch={arch}", when=f"cuda_arch={arch}")
-    depends_on("mfem +cuda+mpi", when="+cuda")
-    depends_on("mfem +rocm+mpi", when="+rocm")
-    depends_on("mfem +umpire", when="+cuda")
-    depends_on("mfem +umpire", when="+rocm")
+    depends_on("hypre+cuda+mpi+umpire", when="+cuda")
+    depends_on("hypre~cuda", when="~cuda")
+    depends_on("mfem+cuda+mpi+umpire", when="+cuda")
+    depends_on("mfem~cuda", when="~cuda")
 
-    depends_on("hypre +rocm+mpi", when="+rocm")
-    requires("+rocm", when="^hypre+rocm")
-    for target in ("none", "gfx803", "gfx900", "gfx906", "gfx908", "gfx90a", "gfx942"):
-        depends_on(f"hypre amdgpu_target={target}", when=f"amdgpu_target={target}")
-        depends_on(f"mfem amdgpu_target={target}", when=f"amdgpu_target={target}")
+    for sm_ in CudaPackage.cuda_arch_values:
+        depends_on("hypre cuda_arch={0}".format(sm_), when="cuda_arch={0}".format(sm_))
+        depends_on("mfem cuda_arch={0}".format(sm_), when="cuda_arch={0}".format(sm_))
+        depends_on("umpire cuda_arch={0}".format(sm_), when="cuda_arch={0}".format(sm_))
 
-    depends_on("hypre+umpire", when="+cuda")
-    depends_on("hypre+umpire", when="+rocm")
+    depends_on("hypre+rocm+mpi+umpire", when="+rocm")
+    depends_on("hypre~rocm", when="~rocm")
+    depends_on("mfem+rocm+mpi+umpire", when="+rocm")
+    depends_on("mfem~rocm", when="~rocm")
+    
+    for arch in ROCmPackage.amdgpu_targets:
+        depends_on("hypre amdgpu_target={0}".format(arch), when="amdgpu_target={0}".format(arch))
+        depends_on("mfem amdgpu_target={0}".format(arch), when="amdgpu_target={0}".format(arch))
+        depends_on("umpire amdgpu_target={0}".format(arch), when="amdgpu_target={0}".format(arch))
+
     depends_on("hypre+gpu-aware-mpi", when="+gpu-aware-mpi")
 
     def setup_run_environment(self, env):
