b70dd96946127f75a6dbdc605788b5be1a25eab7
Michael McKinsey <MichaelMcKinsey1@gmail.com>
2026-04-09 16:05:16 -0700

    Update versions for `raja-perf` and `raja` (#1305)
    * Update package.py
    
    * Update experiment.py
    
    * Update package.py
    
    * Update package.py

--- repos/spack_repo/benchpark/packages/raja/package.py
+++ repos/spack_repo/benchpark/packages/raja/package.py
@@ -10,6 +10,12 @@ from spack_repo.builtin.packages.raja.package import Raja as BuiltinRaja
 class Raja(BuiltinRaja):
 
     version(
+        "2025.12.2",
+        tag="v2025.12.2",
+        commit="eca7c5015a5cf8bf7cc8ad1829fd36d3276ab274",
+        submodules=False,
+    )
+    version(
         "2025.12.1",
         tag="v2025.12.1",
         commit="3b8b59a1e9be2e1066c0d77372b3bf5956e6d6e2",
