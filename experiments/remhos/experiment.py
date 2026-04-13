15e8589e8dbb98983b1191b01c4d4ee9470ba883
Riyaz Haque <your-email@example.com>
2026-04-13 01:43:29 -0700

    Add raja variant

--- experiments/remhos/experiment.py
+++ experiments/remhos/experiment.py
@@ -42,6 +42,13 @@ class Remhos(
         description="Use GPU-aware MPI",
     )
 
+    variant(
+        "raja",
+        default=True,
+        values=(True, False),
+        description="Use RAJA backend for MFEM",
+    )
+
     maintainers("rfhaque")
 
     def compute_applications_section(self):
@@ -133,9 +140,12 @@ class Remhos(
 
     def compute_package_section(self):
         gam = "~gpu-aware-mpi"
+        raja = "~raja"
         if self.spec.satisfies("+cuda") or self.spec.satisfies("+rocm"):
             if self.spec.satisfies("+gpu-aware-mpi"):
                 gam = "+gpu-aware-mpi"
+        if self.spec.satisfies("+raja"):
+            raja = "+raja"
         self.add_package_spec(
-            self.name, [f"remhos{self.determine_version()} +metis {gam}"]
+            self.name, [f"remhos{self.determine_version()} +metis {gam} {raja}"]
         )
