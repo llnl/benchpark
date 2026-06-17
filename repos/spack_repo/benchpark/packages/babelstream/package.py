7626e8dd73b43cd8d42b76e89716672c61084f69
pearce8 <pearce8@llnl.gov>
2026-06-17 14:18:10 -0500

    Clean up package.py

--- repos/spack_repo/benchpark/packages/babelstream/package.py
+++ repos/spack_repo/benchpark/packages/babelstream/package.py
@@ -13,9 +13,6 @@ from spack_repo.builtin.packages.babelstream.package import (
 
 
 class Babelstream(BuiltinBabelstream):
-    git = "https://github.com/UoB-HPC/BabelStream.git"
-
-    version("main", branch="main")
 
     variant("caliper", default=False, description="Enable/disable Caliper support")
 
