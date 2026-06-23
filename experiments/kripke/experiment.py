f79de6621770016ab658ec75c46f317df67dbd19
Joshua Merritt Vining <vining1@llnl.gov>
2026-06-23 09:27:17 -0700

    Test change to kripke to test CI

--- experiments/kripke/experiment.py
+++ experiments/kripke/experiment.py
@@ -2,6 +2,7 @@
 # Benchpark Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: Apache-2.0
+#test
 
 from benchpark.caliper import Caliper
 from benchpark.directives import maintainers, variant
