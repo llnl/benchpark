a22b29ae924b80ea74a1eb51cee929e80d96b1d7
Joshua Merritt Vining <vining1@llnl.gov>
2026-06-24 10:42:05 -0700

    Removed test change from osu-micro-benchmarks experiment.py

--- experiments/osu-micro-benchmarks/experiment.py
+++ experiments/osu-micro-benchmarks/experiment.py
@@ -2,8 +2,6 @@
 # Benchpark Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: Apache-2.0
-#
-# test change
 
 from benchpark.directives import maintainers, variant
 from benchpark.experiment import Experiment
