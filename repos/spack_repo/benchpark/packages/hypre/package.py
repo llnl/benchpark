5948890b7d07355668433f7afdeda4b1fca12004
Riyaz Haque <your-email@example.com>
2026-07-09 19:34:21 -0700

    spack package changes for AMG2023 and hypre

--- repos/spack_repo/benchpark/packages/hypre/package.py
+++ repos/spack_repo/benchpark/packages/hypre/package.py
@@ -67,7 +67,24 @@ class Hypre(BuiltinHypre):
             env.append_flags("LDFLAGS", f"-L{spec['lapack'].prefix}/lib -l{libsci_name}")
 
 class CMakeBuilder(HypreCMakeBuilder):
-    pass
+    root_cmakelists_dir = "src"
+
+    def cmake_args(self):
+        args = super().cmake_args()
+
+        if "+umpire" in self.spec:
+            umpire = self.spec["umpire"]
+            umpire_opts = umpire.prefix.include
+            umpire_libs = umpire.libs
+            if "^camp" in umpire:
+                umpire_libs += umpire["camp"].libs
+            if "^fmt" in umpire:
+                umpire_libs += umpire["fmt"].libs
+
+            args.append(self.define("TPL_UMPIRE_INCLUDE_DIRS", umpire_opts))
+            args.append(self.define("TPL_UMPIRE_LIBRARIES", umpire_libs))
+
+        return args
 
 class AutotoolsBuilder(HypreAutotoolsBuilder):
     pass
