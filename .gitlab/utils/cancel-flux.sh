1afa7835ab94819ce8d18ab678a643871fc77ae2
Joshua Merritt Vining <vining1@llnl.gov>
2026-07-13 14:20:35 -0700

    removed silencing from rmdir. removed lock behavior from cleanupjob

--- .gitlab/utils/cancel-flux.sh
+++ .gitlab/utils/cancel-flux.sh
@@ -26,10 +26,10 @@ echo "CLEANING CHECK"
 if ! $NO_CLEAN; then
     CLEANUP_CI_BUILDS_DIR="${CUSTOM_CI_BUILDS_DIR/#\$HOME/$HOME}"
     CLEANUP_LOCK_DIR="${CLEANUP_CI_BUILDS_DIR}.cleanup.lock"
-    if mkdir "$CLEANUP_LOCK_DIR" 2>/dev/null; then
+    if mkdir "$CLEANUP_LOCK_DIR"; then
         echo "Removing $CLEANUP_CI_BUILDS_DIR"
         rm -rf "$CLEANUP_CI_BUILDS_DIR"
-        rmdir "$CLEANUP_LOCK_DIR" 2>/dev/null || true
+        rmdir "$CLEANUP_LOCK_DIR" || true
     else
         echo "Cleanup already running for $CLEANUP_CI_BUILDS_DIR"
     fi
