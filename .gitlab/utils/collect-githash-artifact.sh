37aee722d3277d75ad68c307c7269f8d0f1707a3
Joshua Merritt Vining <vining1@llnl.gov>
2026-07-16 10:31:33 -0700

    Created a general purpose artifact fetching script. Moved githash and cali specific things into their own specific collection scripts.

--- .gitlab/utils/collect-githash-artifact.sh
+++ .gitlab/utils/collect-githash-artifact.sh
@@ -28,16 +28,15 @@ else
 fi
 
 fetch_args=(
-    "${BASELINE_REF}"
-    "${CI_JOB_NAME}"
-    "${HOST}"
-    "${BENCHMARK}"
-    "${baseline_json}"
-    "${CI_PIPELINE_ID}"
-    "${baseline_status_file}"
+    --ref "${BASELINE_REF}"
+    --job-name "${CI_JOB_NAME}"
+    --artifact-path "artifact-githash/${HOST}/${BENCHMARK}/githash_metadata.json"
+    --output-path "${baseline_json}"
+    --exclude-pipeline-id "${CI_PIPELINE_ID}"
+    --status-output-path "${baseline_status_file}"
 )
 
-bash .gitlab/utils/fetch-githash-artifact.sh "${fetch_args[@]}"
+bash .gitlab/utils/fetch-job-artifact.sh "${fetch_args[@]}"
 
 if [[ "$(cat "${run_status_file}")" == "0" ]]; then
     run_status="PASSED"
